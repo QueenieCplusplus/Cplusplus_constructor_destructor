9ba754cbe77780dd50a3430375437b1eb8386ca8
Queenie Chen <58072875+QueenieCplusplus@users.noreply.github.com>
2019-11-26 10:33:22 +0800

    Add files via upload

---
+++ Constructor.cpp
@@ -0,0 +1,24 @@
+ // 2019, 1126, 10:30, by Queenie
+ // Constructor
+ // 初始化私有物件變數容器
+ 
+ class ShareMarket{
+
+   private:
+        char target[10];
+        int shares;
+        float share_val;
+        double total_val;
+        void know_totoal(){
+          total_val= shares * share_val;
+        }
+
+   public:
+        void Stock(){}; // 建構函數 
+        void before(const char *com, int n, double pr);
+        void buy(int shareAmount, double dealPrice);
+        void sell(int shareAmount, double dealPrice);
+        void after(double dealPrice);
+        void show();
+          
+};
